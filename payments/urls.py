33c14bdf1cf45207d068f4926e7cc683e12ee692
rigstire <brodsky.nick@gmail.com>
2025-04-14 20:46:09 -0700

    paypal

--- payments/urls.py
+++ payments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import process_payment, payment_view
+from .views import process_payment, payment_view, paypal_confirm
 
 urlpatterns = [
     path("process-payment/", process_payment, name="process_payment"),
-    path("payments/", payment_view, name="payments")
+    path("payments/", payment_view, name="payments"),
+     path('paypal-confirm/', paypal_confirm, name='paypal_confirm'),
 ]
