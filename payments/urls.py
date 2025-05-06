072d4b8429d466f8d34a077188f9cce6c4063754
rigstire <brodsky.nick@gmail.com>
2025-05-05 22:52:44 -0700

    msg

--- payments/urls.py
+++ payments/urls.py
@@ -1,9 +1,10 @@
 # payments/urls.py
 from django.urls import path
-from .views import create_order, payment_success, payment_cancel
+from .views import create_order, payment_success, payment_cancel, payments
 
 urlpatterns = [
     path('create-order/', create_order, name='create_order'),
     path('payment/success/', payment_success, name='payment_success'),
     path('payment/cancel/', payment_cancel, name='payment_cancel'),
+    path('payments/payments',payments,name="payments");
 ]
