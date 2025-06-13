5965d1d93174132de00dcd41fe2f03d9fb3f6956
rigstire <brodsky.nick@gmail.com>
2025-06-13 09:09:12 -0700

    chill out

--- payments/urls.py
+++ payments/urls.py
@@ -1,10 +1,12 @@
 # payments/urls.py
 from django.urls import path
-from .views import create_order, payment_success, payment_cancel, payments
+from .views import create_order, create_payment_intent, payment_success, payment_cancel, payments, stripe_webhook
 
 urlpatterns = [
-    path('create-order/', create_order, name='create_order'),
+    path('create-order/', create_order, name='create_order'),  # Legacy endpoint
+    path('create-payment-intent/', create_payment_intent, name='create_payment_intent'),  # New Stripe endpoint
     path('payment/success/', payment_success, name='payment_success'),
     path('payment/cancel/', payment_cancel, name='payment_cancel'),
     path('payments/payments', payments, name="payments"),
+    path('stripe/webhook/', stripe_webhook, name='stripe_webhook'),
 ]
